0add9ee606a6d7dd00086d72f36657410f1fdb7a
Patrick Hardy <patrickwhardy@gmail.com>
2018-07-11 16:15:36 -0600

    put spider in state and make him walk

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
-  ViroText,
   ViroMaterials,
   ViroBox,
   Viro3DObject,
@@ -14,57 +13,61 @@ import {
   ViroNode,
   ViroAnimations,
 } from 'react-viro';
-var createReactClass = require('create-react-class');
-var HelloWorldSceneAR = createReactClass({
-  getInitialState() {
-    return {
-      text : "Initializing AR...",
-      runAnimation: true
-    };
-  },
 
-  render: function() {
-    return (
-      <ViroARScene onTrackingInitialized={()=>{this.setState({text : "Hello World!"})}}>
-        <ViroText text={this.state.text} scale={[.1, .1, .1]} height={1} width={4} position={[0, .5, -1]} style={styles.helloWorldTextStyle} />
+class HelloWorldSceneAR extends Component {
+  
+// const defaultSpider = {
+//     animation: {name: 'walk', run: true, loop: true}
+//   }
+  
+state = {
+  spiders: [
+    {
+      animation: {name: 'walk', run: true, loop: true}
+    }
+  ],  
+}
+  
+  killSpider = (index) => {
+    const spiders = this.state.spiders;
+    spiders[index].animation = {name: 'die', run: true, loop: false}
+    this.setState({
+      spiders
+    })
+  }
 
-        <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-
-        <Viro3DObject
-          source={require('./res/emoji_smile/emoji_smile.vrx')}
-          position={[0, 0, -1]}
-          scale={[.2, .2, .2]}
-          type="VRX"
-          dragType="FixedDistance" onDrag={()=>{}}
-        />
-
+  render() {
+    const spiders = this.state.spiders.map((spider, index) => (
+      <ViroNode
+        key={index}
+        animation={{name: 'advance', run: true, loop: true}}>
         <Viro3DObject
           source={require('./res/PB_Spider/spider.vrx')}
           position={[-1, 0, -1]}
           scale={[.001, .001, .001]}
           type="VRX"
-          animation={{name: 'walk', run: true, loop: true}}
+          animation={spider.animation}
           dragType="FixedDistance" onDrag={()=>{}}
+          onClick={()=>{this.killSpider(index)}}
         />
+      </ViroNode>
+    ))
 
+    return (
+      <ViroARScene onTrackingInitialized={()=>{console.log('hi')}}>
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+        {spiders}
       </ViroARScene>
     );
-  },
-});
+  }
+}
 
 ViroAnimations.registerAnimations({
-  // p: {properties: {positionY: "+=0.3"}, duration:4000}
+  advance: {properties: {positionZ: "+=0.3"}, duration:2000}
 });
 
 var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 50,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
 });
 
 module.exports = HelloWorldSceneAR;
